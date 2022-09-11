2c82e1e20f1809ad13eaa08cf2c48b4fda0e10c3
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-09-11 22:38:25 +0900

    update abc158c

--- abc158/c/main.cpp
+++ abc158/c/main.cpp
@@ -9,18 +9,11 @@ int main() {
   vector<int> x;
   int start = floor(a / 0.08);
   int end = floor((a + 1) / 0.08);
-  // cout << start << endl;
-  // cout << end << endl;
-  for(int i = start; i <= 100; ++i) {
-    if (floor(i * 0.08) >= (a + 1) || floor(i * 0.1) >= (b + 1)) {
-      cout << "-1" << endl;
-      return 0;
-    }
+  for(int i = start; i < floor((a + 1) / 0.08); ++i) {
     if (floor(i * 0.1) == b) {
       cout << i << endl;
       return 0;
     }
   }
   cout << result << endl;
-  // cout << floor(3 / 0.08) << endl;
 }
