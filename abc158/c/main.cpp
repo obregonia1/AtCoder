36ef7c7b304a4fb18bd9f8dee1248a7e7c979ac3
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-09-11 22:46:09 +0900

    update abc158c

--- abc158/c/main.cpp
+++ abc158/c/main.cpp
@@ -1,19 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep(i, n) for (int i = 1; i < (n); ++i)
 
 int main() {
   int a, b;
   cin >> a >> b;
-  string result = "-1";
-  vector<int> x;
-  int start = floor(a / 0.08);
-  int end = floor((a + 1) / 0.08);
-  for(int i = start; i < floor((a + 1) / 0.08); ++i) {
-    if (floor(i * 0.1) == b) {
+  rep(i, 10001) {
+    int ai = i * 8 / 100;
+    int bi = i * 10 / 100;
+    if (a == ai && b == bi) {
       cout << i << endl;
       return 0;
     }
   }
-  cout << result << endl;
+  cout << -1 << endl;
 }
