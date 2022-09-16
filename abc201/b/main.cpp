8fd8502463d859199227e9183a116986a718a74a
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-09-17 08:03:03 +0900

    update abc201b

--- abc201/b/main.cpp
+++ abc201/b/main.cpp
@@ -5,23 +5,8 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  vector<string> s(n);
-  vector<int> t(n);
-  rep(i, n) {
-    string si;
-    int ti;
-    cin >> si >> ti;
-    s[i] = si;
-    t[i] = ti;
-  } 
-  vector<int> tcopy(n);
-  copy(t.begin(), t.end(), back_inserter(tcopy));
-  sort(tcopy.begin(), tcopy.end(), greater<int>());
-  int hi = tcopy[1];
-  int index = 0;
-  rep(i, n) {
-    if (t[i] == hi) index = i;
-  }
-
-  cout << s[index] << endl;
+  vector<pair<int, string>> p(n);
+  rep(i, n) cin >> p[i].second >> p[i].first;
+  sort(p.begin(), p.end());
+  cout << p[p.size() - 2].second << endl;
 }
