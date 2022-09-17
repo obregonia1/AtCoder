644d75ee6d9f76f63d55c0c3c9b3247bda775157
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-09-18 08:16:21 +0900

    add abc269a

---
+++ abc269/a/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i,a, b) for (int i = a; i < (b); ++i)
+
+int main() {
+  int a, b, c, d;
+  cin >> a >> b >> c >> d;
+  
+  cout << (a + b) * (c - d) << endl;
+  cout << "Takahashi" << endl;
+}
