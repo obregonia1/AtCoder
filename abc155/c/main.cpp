1bfd7bab35d8e5f70844c4b195f0ea131c60c278
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-10-01 16:53:42 +0900

    add abc155c

---
+++ abc155/c/main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  map<string, int> s;
+  rep(i, 0, n) {
+   string si;
+   cin >> si;
+   s[si]++;
+  }
+  int ma = 0;
+  for(auto& si : s) {
+    ma = max(ma, si.second);
+  }
+  vector<string> vec;
+  
+  for(auto& si : s) {
+    if (si.second == ma) vec.push_back(si.first);
+  }
+  sort(vec.begin(), vec.end());
+  for (auto& vi : vec) cout << vi << endl;
+  
+  // cout << si.first << si.second << endl;
+
+  
+  // cout << result << endl;
+}
