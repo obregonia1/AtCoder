21daa5564eb9234a21e37d6534c064248d9297f6
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-10-02 08:06:29 +0900

    update abc271c

--- abc271/c/main.cpp
+++ abc271/c/main.cpp
@@ -5,24 +5,15 @@ using namespace std;
 int main() {
   int n;
   cin >> n;
-  vector<long long> a(n);
-  for(auto& ai : a) {
-    cin >> ai;
-  }
-  sort(a.begin(), a.end());
-  int result = 0;
-  int count = 0;
+  vector<int> set(n + 2);
   rep(i, 0, n) {
-    if (a.size() - i > count && a[i] == i + 1) {
-      result = i + 1;
-    }
-    else if (a.size() - count - 1 >= 2) {
-      result = i + 1;
-      count += 2;
-    }
-    else {
-      cout << result << endl;
-      return 0;
-    }
+    int a;
+    cin >> a;
+    set[min(n + 1, a)] = 1;
+  }
+
+  for (int read = 1; n >= 0; ++read) {
+    n -= set[read] ? 1 : 2;
+    if (n < 0) cout << read - 1 << endl;
   }
 }
