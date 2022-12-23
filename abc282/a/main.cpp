2075af60387359e82ab0275d6d76cb7e9ef8c021
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2022-12-23 17:36:35 +0900

    add abc282a

---
+++ abc282/a/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  char c = 'A';
+  rep(i, 0, n) {
+    cout << c; 
+    c++;
+  }
+  cout << "\n";
+
+}
+
