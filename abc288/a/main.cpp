de986084d0285404913f9181aa24cf6070f354cf
Kentaro Takeyama <n.y.skate.of.minds@gmail.com>
2023-02-05 09:31:12 +0900

    add abc288a

---
+++ abc288/a/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+#define rep(i, a, b) for (int i = a; i < (b); ++i)
+
+int main() {
+  int n;
+  cin >> n;
+  rep (i, 0, n) {
+    ll a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+  }
+}
+
